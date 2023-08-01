60329d854406b5741fe22f42517b1a7456b15874
alyaaminuddin <121539265+alyaaminuddin@users.noreply.github.com>
2023-08-01 17:18:50 +0800

    Update employeeretentionapp_h.py

--- employeeretentionapp_h.py
+++ employeeretentionapp_h.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import base64
+import sklearn
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
